b6120ed7081e01cbb98d34db539277fc7867af79
Nerea <801950@unizar.es>
2024-01-24 20:09:38 +0100

    start again

---
+++ src/course_assignment.py
@@ -0,0 +1,10 @@
+# Computer Vision Course Assignment
+# Author: Nerea Gallego (801950)
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+import scipy as sc
+import scipy.optimize as scOptim
+
